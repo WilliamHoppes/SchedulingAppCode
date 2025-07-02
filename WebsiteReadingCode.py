db7a0f599b4905126259a6d3c469d903dceafe2b
William Hoppes <hoppesbeta@gmail.com>
2025-07-01 22:59:00 -0500

    WebsiteReadingCode now has try-except and error recording.

--- WebsiteReadingCode.py
+++ WebsiteReadingCode.py
@@ -5,13 +5,21 @@ import io
 import pandas as pd
 import time
 import csv
+from datetime import date
 
 #https://ai.google.dev/gemini-api/docs
 my_api_key=os.environ['GEMINI_API_KEY']
 
 open_path="C:/Users/hoppe/Documents/SchedulingApp/SchedulingAppLive/01Raw_Website_Snapshots/"
 exit_path="C:/Users/hoppe/Documents/SchedulingApp/SchedulingAppLive/02Processed_Website_Schedules/"
-error_path="C:/Users/hoppe/Documents/SchedulingApp/SchedulingAppLive/"
+error_path="C:/Users/hoppe/Documents/SchedulingApp/SchedulingAppLive/Errors/"
+
+#initialize lists for error log
+Error_file=[]
+Error_type=[]
+Error_date=[]
+Error_message=[]
+current_date=str(date.today())
 
 client = genai.Client(api_key=my_api_key)
 
@@ -37,43 +45,64 @@ for file in os.listdir(open_path):
     file_name=file[:-4]
     output_file_name=exit_path+file[:-4]+".csv"
     while attempts_remaining_counter>0 and os.path.isfile(output_file_name)==False:
-        #Get the image
-        current_image=client.files.upload(file=open_path+file)
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=[current_image, prompt+error_prompt_addon],
-            #https://ai.google.dev/gemini-api/docs/thinking
-            config=types.GenerateContentConfig(
-                thinking_config=types.ThinkingConfig(thinking_budget=0)
-            )
-        )
-        csv_raw_data=str(response.text)
-        #Basic Cleaning
-        csv_raw_data=csv_raw_data.replace("``` csv", "")     #The model insists on inlcuding this
-        csv_raw_data=csv_raw_data.replace("csv", "") 
-        safe_characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*(),   \n"
-        csv_clean_data=""
-        for char in csv_raw_data:
-            if char in safe_characters:
-                csv_clean_data+=char
-        #We need to add pandas for file checking
-        #https://www.statology.org/pandas-read-csv-from-string/
         try:
-            df=pd.read_csv(io.StringIO(csv_clean_data), sep=",")
-            df["Location"]=file[:-4]
-            df.to_csv(output_file_name, index=False)
-            print(file[:-4]+" successful")
-            attempts_remaining_counter=0
+        #Get the image
+            current_image=client.files.upload(file=open_path+file)
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=[current_image, prompt+error_prompt_addon],
+                #https://ai.google.dev/gemini-api/docs/thinking
+                config=types.GenerateContentConfig(
+                    thinking_config=types.ThinkingConfig(thinking_budget=0)
+                )
+            )
+            csv_raw_data=str(response.text)
+            #Basic Cleaning
+            csv_raw_data=csv_raw_data.replace("``` csv", "")     #The model insists on inlcuding this
+            csv_raw_data=csv_raw_data.replace("csv", "") 
+            safe_characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*(),   \n"
+            csv_clean_data=""
+            for char in csv_raw_data:
+                if char in safe_characters:
+                    csv_clean_data+=char
+            #We need to add pandas for file checking
+            #https://www.statology.org/pandas-read-csv-from-string/
+            try:
+                df=pd.read_csv(io.StringIO(csv_clean_data), sep=",")
+                df["Location"]=file[:-4]
+                df.to_csv(output_file_name, index=False)
+                print(file[:-4]+" successful")
+                attempts_remaining_counter=0
+            except Exception as e:
+                print(file+" failed. "+str(attempts_remaining_counter)+" attempts remaining")
+                print("Error code: "+str(e))
+                error_prompt_addon+="The previous attempt failed. While trying to convert this to a pandas dataframe in python, the following error occured: "
+                error_prompt_addon+=str(e)
+                attempts_remaining_counter=attempts_remaining_counter-1
+                Error_file.append(output_file_name)
+                Error_type.append("Failed at csv step")
+                Error_date.append(current_date)
+                Error_message.append(str(e))
+                time.sleep(60)
+#                if attempts_remaining_counter==0:
+#                    with open(exit_path+file[:-4]+"ERROR"+".csv", "w") as file:
+#                        file.write(csv_clean_data)
         except Exception as e:
-            print(file+" failed. "+str(attempts_remaining_counter)+" attempts remaining")
+            print("Gemini model failed!")
             print("Error code: "+str(e))
-            error_prompt_addon+="The previous attempt failed. While trying to convert this to a pandas dataframe in python, the following error occured: "
-            error_prompt_addon+=str(e)
             attempts_remaining_counter=attempts_remaining_counter-1
+            Error_file.append(output_file_name)
+            Error_type.append("Failed at csv step")
+            Error_date.append(current_date)
+            Error_message.append(str(e))
             time.sleep(60)
-#            if attempts_remaining_counter==0:
-#                with open(exit_path+file[:-4]+"ERROR"+".csv", "w") as file:
-#                    file.write(csv_clean_data)
         time.sleep(60)
 
+df=pd.DataFrame({
+    "Error_file": Error_file,
+    "Error_type": Error_type,
+    "Error_date": Error_date,
+    "Error_message": Error_message
+})
 
+df.to_excel(error_path+"Errors.xlsx", index=False)
--- WebsiteSnapshotCode.py
+++ WebsiteSnapshotCode.py
@@ -14,7 +14,7 @@ output_path = "C:/Users/hoppe/Documents/SchedulingApp/SchedulingAppLive/01Raw_We
 file_df=pd.read_csv("C:/Users/hoppe/Documents/SchedulingApp/SchedulingAppCode/List_of_Event_Locations.csv")
 
 for f in os.listdir(output_path):
-    print(f)
+    print(output_path+f)
 
 def capture_website_screenshot(url, output_path):
     chrome_options = Options()
@@ -65,24 +65,3 @@ for i in range(len(file_urls)):
         capture_website_screenshot(file_urls[i]+current_year+"-"+current_month_fixed+"/", output_path=output_path+file_names[i]+".png")
     time.sleep(5)
 
-#Tech
-#capture_website_screenshot("https://iondistrict.com/events/month/"+current_year+"-"+current_month_fixed+"/", output_path=output_path+"IonDistrict.png")
-#capture_website_screenshot("https://www.tmc.edu/innovation/events/list/", output_path=output_path+"TMCi.png")
-#capture_website_screenshot("https://www.meetup.com/houston-ai-club/", output_path=output_path+"HoustonAiClub.png")#
-
-#Fun
-#capture_website_screenshot("https://www.meetup.com/houston-reading-parties/", output_path=output_path+"HoustonReadingParties.png")
-#capture_website_screenshot("https://www.threads.net/@sbchoustoncentral", output_path=output_path+"SilentReadingGroup.png")
-#capture_website_screenshot("https://continentalclub.com/houston", output_path=output_path+"ContinentalClub.png")
-#capture_website_screenshot("https://www.thehobbycenter.org/events/month/"+current_year+"-"+current_month_fixed+"/", output_path=output_path+"HobbyCenter.png")
-#capture_website_screenshot("https://www.milleroutdoortheatre.com/performance-list/", output_path=output_path+"MillerOutdoorTheatre.png")
-#capture_website_screenshot("https://houstonsymphony.org/performance-calendar/", output_path=output_path+"HoustonSymphony.png")
-#capture_website_screenshot("https://www.houstondynamofc.com/schedule/#date=2025-04-15", output_path=output_path+"HoustonDynamo.png")
-
-#Fun, the Post chunk
-#capture_website_screenshot("https://www.posthtx.com/event?location=Art+Club", output_path=output_path+"POST_ArtClub.png")
-#capture_website_screenshot("https://www.posthtx.com/event?location=POST+Market", output_path=output_path+"POST_Market.png")
-#capture_website_screenshot("https://www.posthtx.com/event?location=Rooftop", output_path=output_path+"POST_Rooftop.png")
-#capture_website_screenshot("https://www.posthtx.com/event?location=X+Atrium", output_path=output_path+"POST_XAtrium.png")
-#capture_website_screenshot("https://www.posthtx.com/event?location=Z+Atrium", output_path=output_path+"POST_ZAtrium.png")
-#capture_website_screenshot("https://www.posthtx.com/event?location=713+Music+Hall", output_path=output_path+"POST_713MusicHall.png")
